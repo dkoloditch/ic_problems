c9839e1e5231be921c9459369b3c00085c6a737d
Derek Koloditch <derek.koloditch@gmail.com>
2017-04-14 15:01:00 -0600

    product of three second attempt to improve big o

--- javascript/product_of_three/product_of_three.js
+++ javascript/product_of_three/product_of_three.js
@@ -7,12 +7,34 @@ function productOfThree(arrayOfInts) {
     throw new Error("Input must have 3 or more integers.")
   }
 
-  arrayOfInts.sort(function(a,b){return b-a});
-  const topThree = arrayOfInts.splice(0, 3);
-  return topThree.reduce(function(sum, n){return sum *= n})
+  let current;
+  let nSmall  = arrayOfInts[0] - 2;
+  let nMedium = arrayOfInts[0] - 1;
+  let nLarge  = arrayOfInts[0];
+
+  for (i = 1; i < arrayOfInts.length; i++) {
+    current = arrayOfInts[i];
+
+    if (current > nLarge) {
+      nSmall  = nMedium;
+      nMedium = nLarge;
+      nLarge  = current;
+    }
+    else if (current > nMedium && current > nSmall) {
+      nSmall  = nMedium;
+      nMedium = current;
+    }
+    else if (current > nSmall) {
+      nSmall  = current;
+    }
+  };
+
+  return [nSmall, nMedium, nLarge].reduce(function(sum, n){return sum *= n})
 }
 
 console.log(productOfThree([1,2]) === undefined);
 console.log(productOfThree([1,2,3]) === 6);
 console.log(productOfThree([1,2,3,4]) === 24);
 console.log(productOfThree([2,4,3,1]) === 24);
+console.log(productOfThree([2,-4,3,1]) === 6);
+console.log(productOfThree([-3,-1,-2]) === -6);
