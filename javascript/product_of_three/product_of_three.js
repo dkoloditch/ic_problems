2ae16126c25dcbe3816fe120aafd0986af1c8207
Derek Koloditch <derek.koloditch@gmail.com>
2017-04-14 17:18:22 -0600

    product of three third attempt based on ideal solution

--- javascript/product_of_three/product_of_three.js
+++ javascript/product_of_three/product_of_three.js
@@ -3,38 +3,52 @@
 
 function productOfThree(arrayOfInts) {
   if (arrayOfInts.length < 3) {
-    return;
     throw new Error("Input must have 3 or more integers.")
   }
 
+  const first  = arrayOfInts[0];
+  const second = arrayOfInts[1];
+  const third  = arrayOfInts[2];
   let current;
-  let nSmall  = arrayOfInts[0] - 2;
-  let nMedium = arrayOfInts[0] - 1;
-  let nLarge  = arrayOfInts[0];
+  let highest = Math.max(first, second);
+  let lowest  = Math.min(first, second);
+  let highestProductOf3 = first * second * third;
+  let highestProductOf2 = first * second;
+  let lowestProductOf2  = first * second;
 
-  for (i = 1; i < arrayOfInts.length; i++) {
+  for (let i = 2; i < arrayOfInts.length; i++) {
     current = arrayOfInts[i];
 
-    if (current > nLarge) {
-      nSmall  = nMedium;
-      nMedium = nLarge;
-      nLarge  = current;
-    }
-    else if (current > nMedium && current > nSmall) {
-      nSmall  = nMedium;
-      nMedium = current;
-    }
-    else if (current > nSmall) {
-      nSmall  = current;
-    }
+    highestProductOf3 = Math.max(
+      highestProductOf3,
+      highestProductOf2 * current,
+      lowestProductOf2 * current
+    );
+
+    highestProductOf2 = Math.max(
+      highestProductOf2,
+      highest * current,
+      lowest * current
+    );
+
+    lowestProductOf2 = Math.min(
+      lowestProductOf2,
+      highest * current,
+      lowest * current
+    );
+
+    highest = Math.max(current, highest);
+
+    lowest  = Math.min(current, lowest);
   };
 
-  return [nSmall, nMedium, nLarge].reduce(function(sum, n){return sum *= n})
+  return highestProductOf3;
 }
 
-console.log(productOfThree([1,2]) === undefined);
 console.log(productOfThree([1,2,3]) === 6);
 console.log(productOfThree([1,2,3,4]) === 24);
 console.log(productOfThree([2,4,3,1]) === 24);
 console.log(productOfThree([2,-4,3,1]) === 6);
 console.log(productOfThree([-3,-1,-2]) === -6);
+console.log(productOfThree([1, 10, -5, 1, -100]) === 5000);
+console.log(productOfThree([100, 101, -5, 6, -100]) === 60600);
