8e7f94951c08d8b374ed6472aa27fcec2175f932
Derek Koloditch <derek.koloditch@gmail.com>
2017-04-25 14:04:36 -0600

    more elimination test cases and related code

--- javascript/rectangular_love/rectangular_love.js
+++ javascript/rectangular_love/rectangular_love.js
@@ -1,8 +1,15 @@
 "use strict";
 
 const findMatch = (recA, recB) => {
+  // recB too far above recA
   if (recB.bottomY > (recA.bottomY + recA.height)) return {};
+  // recB too far below recA
+  if ((recB.bottomY + recB.height) < recA.bottomY ) return {};
+  // recB too far right of recA
   if (recB.leftX > (recA.leftX + recA.width)) return {};
+  // recB too far left of recA
+  if ((recB.leftX + recB.width) < recA.leftX) return {};
+
 }
 
 // test cases
@@ -34,7 +41,7 @@ console.log(
   )
 );
 
-// no match - rec B outside height of recA
+// no match - recB outside height of recA
 console.log(
   JSON.stringify(
     findMatch(
@@ -58,12 +65,60 @@ console.log(
   )
 );
 
-// no match - rec B outside width of recA
+// no match - recB outside width of recA
 console.log(
   JSON.stringify(
     findMatch(
       {
         leftX:1,
+        bottomY:1,
+        width:1,
+        height:1
+      },
+      {
+        leftX:3,
+        bottomY:1,
+        width:1,
+        height:1
+      }
+    )
+  )
+  ===
+  JSON.stringify(
+    {}
+  )
+);
+
+// no match - recB too far below recA
+console.log(
+  JSON.stringify(
+    findMatch(
+      {
+        leftX:1,
+        bottomY:5,
+        width:1,
+        height:1
+      },
+      {
+        leftX:1,
+        bottomY:3,
+        width:1,
+        height:1
+      }
+    )
+  )
+  ===
+  JSON.stringify(
+    {}
+  )
+);
+
+// no match - recB too far left recA
+console.log(
+  JSON.stringify(
+    findMatch(
+      {
+        leftX:5,
         bottomY:1,
         width:1,
         height:1
